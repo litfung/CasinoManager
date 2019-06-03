56e5bd987fd508a347f117690e77c6c7110e6f0d
Mike Kent <thakfu@hotmail.com>
2019-06-02 21:44:47 -0500

    6/2/19 - 0.055

--- CasinoManager/AdminRegister.cs
+++ CasinoManager/AdminRegister.cs
@@ -26,43 +26,74 @@ namespace CasinoManager
 
         private void btnSubmit_Click(object sender, EventArgs e)
         {
-            
-            if (tbPassword.Text.Length >= 8)
+
+            if (String.IsNullOrWhiteSpace(tbFirst.Text))
             {
-                if (tbPassword.Text == tbConfirmPW.Text)
+                MessageBox.Show("All fields must be filled out");
+            }
+            else if (String.IsNullOrWhiteSpace(tbLast.Text))
+            {
+                MessageBox.Show("All fields must be filled out");
+            }
+            else if (String.IsNullOrWhiteSpace(tbUsername.Text))
+            {
+                MessageBox.Show("All fields must be filled out");
+            }
+            else if (String.IsNullOrWhiteSpace(tbEmail.Text))
+            {
+                MessageBox.Show("All fields must be filled out");
+            }
+            else if (String.IsNullOrWhiteSpace(tbCasino.Text))
+            {
+                MessageBox.Show("All fields must be filled out");
+            }
+            else if (String.IsNullOrWhiteSpace(tbClub.Text))
+            {
+                MessageBox.Show("All fields must be filled out");
+            }
+            else if (String.IsNullOrWhiteSpace(tbLocation.Text))
+            {
+                MessageBox.Show("All fields must be filled out");
+            }
+            else
+            {
+                if (tbPassword.Text.Length >= 8)
                 {
-
-                    Guid userGuid = System.Guid.NewGuid();
-                    string hashedPassword = Security.HashSHA1(tbPassword.Text + userGuid.ToString());
-
-                    casino.firstname = tbFirst.Text;
-                    casino.lastname = tbLast.Text;
-                    casino.username = tbUsername.Text;
-                    casino.password = hashedPassword;
-                    casino.email = tbEmail.Text; ;
-                    casino.casinoname = tbCasino.Text;
-                    casino.clubname = tbClub.Text;
-                    casino.casinolocation = tbLocation.Text;
-                    casino.userguid = userGuid;
-
-                    int rowsc = oper.insertCasino(casino);
-                    if (rowsc > 0)
+                    if (tbPassword.Text == tbConfirmPW.Text)
                     {
-                        Operations.lastFrom = "AdminReg";
-                        acLabel = "You have created a new casino.";
 
-                        AdminCreated adminCreated = new AdminCreated();
-                        adminCreated.Show();
+                        Guid userGuid = System.Guid.NewGuid();
+                        string hashedPassword = Security.HashSHA1(tbPassword.Text + userGuid.ToString());
+
+                        casino.firstname = tbFirst.Text;
+                        casino.lastname = tbLast.Text;
+                        casino.username = tbUsername.Text;
+                        casino.password = hashedPassword;
+                        casino.email = tbEmail.Text; ;
+                        casino.casinoname = tbCasino.Text;
+                        casino.clubname = tbClub.Text;
+                        casino.casinolocation = tbLocation.Text;
+                        casino.userguid = userGuid;
+
+                        int rowsc = oper.insertCasino(casino);
+                        if (rowsc > 0)
+                        {
+                            Operations.lastFrom = "AdminReg";
+                            acLabel = "You have created a new casino.";
+
+                            AdminCreated adminCreated = new AdminCreated();
+                            adminCreated.Show();
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Your passwords do not match");
                     }
                 }
                 else
                 {
-                    MessageBox.Show("Your passwords do not match");
+                    MessageBox.Show("Your password must be at least 8 characters");
                 }
-            }
-            else
-            {
-                MessageBox.Show("Your password must be at least 8 characters");
             }
 
 
